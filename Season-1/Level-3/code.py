f32d5976dd5679e8378fdf5559e755796d398160
David Leon <jelid.leon@gmail.com>
2024-09-15 04:24:40 +0000

    Level-3 completed

--- Season-1/Level-3/code.py
+++ Season-1/Level-3/code.py
@@ -13,6 +13,13 @@ def source():
     TaxPayer('foo', 'bar').get_prof_picture(request.args["input"])
 ### Unrelated to the exercise -- Ends here -- Please ignore
 
+def safe_path(path):
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    filepath = os.path.normpath(os.path.join(base_dir, path))
+    if base_dir != os.path.commonpath([base_dir, filepath]):
+        return None
+    return filepath
+
 class TaxPayer:
 
     def __init__(self, username, password):
@@ -35,8 +42,9 @@ class TaxPayer:
         base_dir = os.path.dirname(os.path.abspath(__file__))
         prof_picture_path = os.path.normpath(os.path.join(base_dir, path))
 
-        with open(prof_picture_path, 'rb') as pic:
-            picture = bytearray(pic.read())
+        if safe_path(prof_picture_path):
+            with open(prof_picture_path, 'rb') as pic:
+                picture = bytearray(pic.read())
 
         # assume that image is returned on screen after this
         return prof_picture_path
@@ -48,8 +56,9 @@ class TaxPayer:
         if not path:
             raise Exception("Error: Tax form is required for all users")
 
-        with open(path, 'rb') as form:
-            tax_data = bytearray(form.read())
+        if safe_path(path):
+            with open(path, 'rb') as form:
+                tax_data = bytearray(form.read())
 
         # assume that tax data is returned on screen after this
         return path
